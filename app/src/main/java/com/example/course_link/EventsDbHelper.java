7d5a076c0b3d8974efbbe8cf658887b8a8dea3c8
jessica <idikejessica@gmail.com>
2025-11-27 04:13:05 -0800

    Event had problems with versions so i changed both Uderdbhelper and EventDBhelper to version 3

--- app/src/main/java/com/example/course_link/EventsDbHelper.java
+++ app/src/main/java/com/example/course_link/EventsDbHelper.java
@@ -3,9 +3,9 @@ package com.example.course_link;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-public class EventsDbHelper extends SQLiteOpenHelper{
+public class EventsDbHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "clublink.db";
-    public static final int DB_VERSION = 2;
+    public static final int DB_VERSION = 3;
 
 
     public static final String T_EVENTS = "events";
@@ -18,6 +18,7 @@ public class EventsDbHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        android.util.Log.d("EventsDbHelper", "onCreate: creating events table");
         db.execSQL("CREATE TABLE " + T_EVENTS + " (" +
                 "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "title TEXT NOT NULL, " +
@@ -27,16 +28,17 @@ public class EventsDbHelper extends SQLiteOpenHelper{
                 "category TEXT, " +
                 "image_url TEXT, " +
                 "is_bookmarked INTEGER DEFAULT 0, " +
-                "is_going INTEGER DEFAULT 0)" );
+                "is_going INTEGER DEFAULT 0)");
 
-    db.execSQL("INSERT INTO events(title, club, start_epoch, location, category, image_url) VALUES" +
-            "('Hack Night', 'CS Club', strftime('%s','now','+3 days')*1000, 'OM 1335', 'Workshops', '')," +
-            "('Pumpkin Social', 'TRU Social', strftime('%s','now','+5 days')*1000, 'Student Union', 'Socials', '')," +
-            "('Intramural Soccer', 'Athletics', strftime('%s','now','+7 days')*1000, 'Hillside Field', 'Sports', '')");
+        db.execSQL("INSERT INTO events(title, club, start_epoch, location, category, image_url) VALUES" +
+                "('Hack Night', 'CS Club', strftime('%s','now','+3 days')*1000, 'OM 1335', 'Workshops', '')," +
+                "('Pumpkin Social', 'TRU Social', strftime('%s','now','+5 days')*1000, 'Student Union', 'Socials', '')," +
+                "('Intramural Soccer', 'Athletics', strftime('%s','now','+7 days')*1000, 'Hillside Field', 'Sports', '')");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        android.util.Log.d("EventsDbHelper", "onUpgrade: " + oldVersion + " -> " + newVersion);
         db.execSQL("DROP TABLE IF EXISTS " + T_EVENTS);
         onCreate(db);
     }
